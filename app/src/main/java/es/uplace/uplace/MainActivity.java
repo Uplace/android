f325868c6892efa58046a8b1b671d91dd69fed87
NicosFabro <nicosfabroc@gmail.com>
2018-03-04 13:14:10 +0100

    Added onClick in btnFilters to open the filters drawer #2

--- app/src/main/java/es/uplace/uplace/MainActivity.java
+++ app/src/main/java/es/uplace/uplace/MainActivity.java
@@ -3,12 +3,15 @@ package es.uplace.uplace;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     DrawerLayout drawerFilters;
     EditText inputTxtMain;
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViews();
+        setClickListeners();
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this, R.array.dwelling_types, android.R.layout.simple_spinner_item);
@@ -34,5 +38,19 @@ public class MainActivity extends AppCompatActivity {
         btnFilters = findViewById(R.id.btnFilters);
         btnSearch = findViewById(R.id.btnSearch);
         spinDwelling = findViewById(R.id.spinDwelling);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnFilters:
+                drawerFilters.openDrawer(Gravity.START);
+//                Toast.makeText(this, "Filters", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    private void setClickListeners() {
+        btnFilters.setOnClickListener(this);
     }
 }
