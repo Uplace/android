6e7e7ffff0cbcb96ce5425ce3fe0acdc4c3ab82a
NicosFabro <nicosfabroc@gmail.com>
2018-03-27 22:33:00 +0200

    Made findAllProperties() with Retrofit #8

--- app/src/main/java/es/uplace/uplace/fragments/ListSearchFragment.kt
+++ app/src/main/java/es/uplace/uplace/fragments/ListSearchFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,6 +17,9 @@ import es.uplace.uplace.domain.Property
 import es.uplace.uplace.domain.enumeration.TransactionType
 import es.uplace.uplace.retrofit.PropertyService
 import es.uplace.uplace.retrofit.Service
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -34,37 +38,45 @@ class ListSearchFragment : Fragment() {
 //
 //        val call = propertyService.findAllProperties()
 //
-//        val response = call.execute()
-//
-//        if (response.isSuccessful) {
-//            val properties = response.body()
+//        val properties = call.execute().body()
 
-        var properties: List<Property>
+        findAllProperties(v)
 
-        properties = mutableListOf(
-                Property("House1", "987654", "Apartment", TransactionType.BUY, 100, Location(1, 1.0, 1.0, "08001", "Barcelona", "Calle House1")),
-                Property("House2", "987654", "Apartment", TransactionType.BUY, 100, Location(1, 1.0, 1.0, "08001", "Barcelona", "Calle House1")),
-                Property("House3", "987654", "Apartment", TransactionType.BUY, 100, Location(1, 1.0, 1.0, "08001", "Barcelona", "Calle House1")),
-                Property("House4", "987654", "Apartment", TransactionType.BUY, 100, Location(1, 1.0, 1.0, "08001", "Barcelona", "Calle House1")))
-                .toList()
-
-//
-            val adapter = ListSearchAdapter(properties)
-            adapter.properties = properties
-            val recyclerProperty = v.findViewById<RecyclerView>(R.id.recyclerProperty)
-            recyclerProperty.adapter = adapter
-            recyclerProperty.layoutManager = LinearLayoutManager(context)
-//        } else {
-//            Toast.makeText(context, "Error al recuperar propiedades", Toast.LENGTH_SHORT).show()
-//        }
-
-//        val recyclerProperty = v.findViewById(R.id.recyclerProperty) as RecyclerView
-//        val layoutManager = LinearLayoutManager(context)
-//        recyclerProperty.layoutManager = layoutManager
-//        recyclerProperty.adapter = listSearchAdapter
-//        listSearchAdapter.setOnClickListener(this)
+//        val adapter = ListSearchAdapter(properties)
+//        adapter.properties = properties
+//        val recyclerProperty = v.findViewById<RecyclerView>(R.id.recyclerProperty)
+//        recyclerProperty.adapter = adapter
+//        recyclerProperty.layoutManager = LinearLayoutManager(context)
 
         return v
+    }
+
+    fun findAllProperties(view: View) {
+        val propertyService = PropertyService.create()
+        val call = propertyService.findAllProperties()
+        Log.d("ncs", "Call: " + call.toString())
+
+        call.enqueue(object : Callback<List<Property>> {
+            override fun onFailure(call: Call<List<Property>>?, t: Throwable?) {
+                Toast.makeText(context, "Error en Callback", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onResponse(call: Call<List<Property>>?, response: Response<List<Property>>?) {
+                if (response != null) {
+
+                    val properties: List<Property> = response.body()!!
+
+                    val adapter = ListSearchAdapter(properties)
+                    adapter.properties = properties
+                    val recyclerProperty = view.findViewById<RecyclerView>(R.id.recyclerProperty)
+                    recyclerProperty.adapter = adapter
+                    recyclerProperty.layoutManager = LinearLayoutManager(context)
+                } else {
+                    Toast.makeText(context, "Response is null", Toast.LENGTH_LONG).show()
+                }
+            }
+
+        })
     }
 
 //    override fun itemClicked(view: View) {
