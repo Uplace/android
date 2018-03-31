f626f2b51d0d097a355383ed96bb1e8a8c79f1f0
nicos <nicosfabroc@gmail.com>
2018-03-31 20:00:20 +0200

    Some changes in Property and it's listCard

--- app/src/main/java/es/uplace/uplace/adapters/ListSearchAdapter.kt
+++ app/src/main/java/es/uplace/uplace/adapters/ListSearchAdapter.kt
@@ -1,5 +1,6 @@
 package es.uplace.uplace.adapters
 
+import android.annotation.SuppressLint
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -23,14 +24,26 @@ class ListSearchAdapter(var properties: List<Property>?) : RecyclerView.Adapter<
         private val txtPrice = view.findViewById<TextView>(R.id.txtPrice)
         private val txtType = view.findViewById<TextView>(R.id.txtType)
         private val txtYearOfConst = view.findViewById<TextView>(R.id.txtYearOfConst)
-        private val txtChar1 = view.findViewById<TextView>(R.id.txtChar1)
+        private val txtSurface = view.findViewById<TextView>(R.id.txtSurface)
         private val txtChar2 = view.findViewById<TextView>(R.id.txtChar2)
         private val txtChar3 = view.findViewById<TextView>(R.id.txtChar3)
 
+        @SuppressLint("SetTextI18n")
         fun bind(property: Property) {
             txtPropertyName.text = property.title
             txtLocation.text = property.location.fullAddress
             txtType.text = property.propertyType
+            txtYearOfConst.text = "${property.yearConstruction}"
+
+            when (property.transaction) {
+                "RENT" -> txtPrice.text = "${property.priceRent}€"
+                "BUY" -> txtPrice.text = "${property.priceSell}€"
+                "TRANSFER" -> txtPrice.text = "${property.priceTransfer}€"
+                "RENT_BUY" -> txtPrice.text = "${property.priceSell}€ - ${property.priceRent}€"
+                else -> txtPrice.text = "undefined"
+            }
+
+            txtSurface.text = "${property.surface}m2"
         }
 
     }
