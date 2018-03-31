c496630f85dc3d7dacd1be70c3835858ee5d0eaf
nicos <nicosfabroc@gmail.com>
2018-03-31 20:14:45 +0200

    Some changes in listCard layout #3

--- app/src/main/java/es/uplace/uplace/adapters/ListSearchAdapter.kt
+++ app/src/main/java/es/uplace/uplace/adapters/ListSearchAdapter.kt
@@ -16,17 +16,13 @@ class ListSearchAdapter(var properties: List<Property>?) : RecyclerView.Adapter<
 
         private val imgProperty = view.findViewById<ImageView>(R.id.imgProperty)
         private val imgFav = view.findViewById<ImageView>(R.id.imgFav)
-        private val imgChar1 = view.findViewById<ImageView>(R.id.imgChar1)
-        private val imgChar2 = view.findViewById<ImageView>(R.id.imgChar2)
-        private val imgChar3 = view.findViewById<ImageView>(R.id.imgChar3)
+        private val txtTransaction = view.findViewById<TextView>(R.id.txtTransactionType)
         private val txtPropertyName = view.findViewById<TextView>(R.id.txtPropertyName)
         private val txtLocation = view.findViewById<TextView>(R.id.txtLocation)
         private val txtPrice = view.findViewById<TextView>(R.id.txtPrice)
         private val txtType = view.findViewById<TextView>(R.id.txtType)
         private val txtYearOfConst = view.findViewById<TextView>(R.id.txtYearOfConst)
         private val txtSurface = view.findViewById<TextView>(R.id.txtSurface)
-        private val txtChar2 = view.findViewById<TextView>(R.id.txtChar2)
-        private val txtChar3 = view.findViewById<TextView>(R.id.txtChar3)
 
         @SuppressLint("SetTextI18n")
         fun bind(property: Property) {
@@ -34,12 +30,16 @@ class ListSearchAdapter(var properties: List<Property>?) : RecyclerView.Adapter<
             txtLocation.text = property.location.fullAddress
             txtType.text = property.propertyType
             txtYearOfConst.text = "${property.yearConstruction}"
+            txtTransaction.text = property.transaction.toLowerCase().capitalize()
 
             when (property.transaction) {
                 "RENT" -> txtPrice.text = "${property.priceRent}€"
                 "BUY" -> txtPrice.text = "${property.priceSell}€"
                 "TRANSFER" -> txtPrice.text = "${property.priceTransfer}€"
-                "RENT_BUY" -> txtPrice.text = "${property.priceSell}€ - ${property.priceRent}€"
+                "RENT_BUY" -> {
+                    txtPrice.text = "${property.priceSell}€ - ${property.priceRent}€"
+                    txtTransaction.text = "Buy or rent"
+                }
                 else -> txtPrice.text = "undefined"
             }
 
