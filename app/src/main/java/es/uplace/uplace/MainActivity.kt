58eedfc4e1b574e744deaee698a46ed2f6aceb47
NicosFabro <nicosfabroc@gmail.com>
2018-04-09 21:16:43 +0200

    Changed main activity and card_list #2 #3

--- app/src/main/java/es/uplace/uplace/MainActivity.kt
+++ app/src/main/java/es/uplace/uplace/MainActivity.kt
@@ -3,13 +3,15 @@ package es.uplace.uplace
 import kotlinx.android.synthetic.main.activity_main.*
 
 import android.content.Intent
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.Button
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,15 +23,26 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     }
 
-    override fun onClick(v: View) {
-        when (v.id) {
-//            R.id.btnFilters -> drawer_layout.openDrawer(Gravity.START)
-            R.id.btnSearch -> startActivity(Intent(this, SearchActivity::class.java))
+    private fun setClickListeners() {
+        btnBuy.setOnClickListener {
+            changeSelected(btnBuy)
+        }
+        btnRent.setOnClickListener {
+            changeSelected(btnRent)
+        }
+        btnTransfer.setOnClickListener {
+            changeSelected(btnTransfer)
         }
     }
 
-    private fun setClickListeners() {
-//        btnFilters.setOnClickListener(this)
-//        btnSearch.setOnClickListener(this)
+    fun changeSelected(btn: Button) {
+        btn.isSelected = !btn.isSelected
+        if (btn.isSelected) {
+            btn.setBackgroundColor(resources.getColor(R.color.primaryDark))
+            btn.setTextColor(Color.WHITE)
+        } else {
+            btn.setBackgroundColor(resources.getColor(R.color.primary))
+            btn.setTextColor(resources.getColor(R.color.textPrimary))
+        }
     }
 }
