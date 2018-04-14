3e73e685fd864234c060856f1bd823141baef131
nicos <nicosfabroc@gmail.com>
2018-04-15 01:03:55 +0200

    Added SearchView in ToolBar #4

--- app/src/main/java/es/uplace/uplace/SearchActivity.kt
+++ app/src/main/java/es/uplace/uplace/SearchActivity.kt
@@ -4,6 +4,10 @@ import kotlinx.android.synthetic.main.activity_search.*
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.support.v7.widget.SearchView
+import android.widget.Toast
 import es.uplace.uplace.adapters.SearchPageAdapter
 
 import es.uplace.uplace.fragments.ListSearchFragment
@@ -21,11 +25,25 @@ class SearchActivity : AppCompatActivity() {
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
         setupViewPager()
+    }
 
-        tabs.setupWithViewPager(container)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        val search: MenuItem = menu!!.findItem(R.id.action_search)
+        val searchView: SearchView = search.actionView as SearchView
+        searchView.setQueryHint("Search");
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
 
-        tabs.getTabAt(0)!!.setIcon(R.drawable.ic_view_list_black)
-        tabs.getTabAt(1)!!.setIcon(R.drawable.ic_map_black)
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                Toast.makeText(applicationContext, "Query text submit", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?) = false
+
+        })
+
+        return super.onCreateOptionsMenu(menu)
     }
 
     fun setupViewPager() {
@@ -38,5 +56,10 @@ class SearchActivity : AppCompatActivity() {
         searchPagerAdapter.fragments = listOf(listSearchFragment, mapFragment)
 
         container.adapter = searchPagerAdapter
+
+        tabs.setupWithViewPager(container)
+
+        tabs.getTabAt(0)!!.setIcon(R.drawable.ic_view_list_black)
+        tabs.getTabAt(1)!!.setIcon(R.drawable.ic_map_black)
     }
 }
