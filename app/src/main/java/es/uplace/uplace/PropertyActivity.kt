d39c3305c499921bc4fe6305dee1385ccfc5b03c
NicosFabro <nicosfabroc@gmail.com>
2018-05-02 18:22:32 +0200

    Tried to add clickListener when click on property card

--- app/src/main/java/es/uplace/uplace/PropertyActivity.kt
+++ app/src/main/java/es/uplace/uplace/PropertyActivity.kt
@@ -1,5 +1,7 @@
 package es.uplace.uplace
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import kotlinx.android.synthetic.main.activity_property.*
 
 import android.support.v7.app.AppCompatActivity
@@ -14,13 +16,20 @@ class PropertyActivity : AppCompatActivity() {
     var propertyCharAdapter = PropertyCharAdapter(this)
     var propertyExtraAdapter = PropertyExtraAdapter(this)
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_property)
 
-        txtName.text = "Test Nombre Largo, 1, 2, 3"
-        txtLocation.text = "Test Location: c/Lorem ipsum 123, Prpl. 4 08001, Barcelona, Spain"
-        txtPrice.text = "Venta: 999999.99€"
+        val intent: Intent = getIntent()
+        val name: String = intent.getStringExtra("name")
+        val location: String = intent.getStringExtra("location")
+        val price: String = intent.getStringExtra("price")
+        val transaction: String = intent.getStringExtra("transaction")
+
+        txtName.text = name
+        txtLocation.text = location
+        txtPrice.text = "$transaction: $price€"
         recyclerChars.layoutManager = GridLayoutManager(this, 2)
         recyclerExtras.layoutManager = GridLayoutManager(this, 2)
         recyclerChars.adapter = propertyCharAdapter
