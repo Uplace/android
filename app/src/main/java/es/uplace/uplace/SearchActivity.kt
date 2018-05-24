53f9e650b6bb6d1261645dcd5bf91ceb98cf2b15
nicos <nicosfabroc@gmail.com>
2018-05-25 01:53:38 +0200

    Revert "Filterdrop unfinished"
    This reverts commit 0659c30

--- app/src/main/java/es/uplace/uplace/SearchActivity.kt
+++ app/src/main/java/es/uplace/uplace/SearchActivity.kt
@@ -7,11 +7,19 @@ import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.support.v7.widget.SearchView
+import android.util.Log
+import android.widget.ProgressBar
 import android.widget.Toast
 import es.uplace.uplace.adapters.SearchPageAdapter
+import es.uplace.uplace.domain.Content
+import es.uplace.uplace.domain.Property
 
 import es.uplace.uplace.fragments.ListSearchFragment
 import es.uplace.uplace.fragments.MapSearchFragment
+import es.uplace.uplace.retrofit.PropertyService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SearchActivity : AppCompatActivity() {
 
@@ -24,7 +32,43 @@ class SearchActivity : AppCompatActivity() {
         setSupportActionBar(toolbarTop)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
-        setupViewPager()
+        findPropertiesByCriteria()
+    }
+
+    private fun findPropertiesByCriteria() {
+
+        val propertyService = PropertyService.create()
+        val params: MutableMap<String, String>? = HashMap()
+        params?.put("city.equals", "Barcelona")
+
+        val paramMap: Map<String, String> = HashMap(params)
+        paramMap.forEach { p -> Log.d("ncs", "$p") }
+
+        val call = propertyService.findAllProperties(paramMap)
+        Log.d("ncs", "Call: " + call.toString())
+
+        call.enqueue(object : Callback<Content> {
+            override fun onFailure(call: Call<Content>?, t: Throwable?) {
+                Toast.makeText(this@SearchActivity, "Error en Callback", Toast.LENGTH_SHORT).show()
+                Log.d("ncs", "Error en Callback: ${t.toString()}")
+                pg_bar.visibility = ProgressBar.GONE
+            }
+
+            override fun onResponse(call: Call<Content>?, response: Response<Content>?) {
+                if (response != null) {
+                    val content: Content? = response.body()
+                    Log.d("ncs", content.toString())
+
+                    val properties = content?.content!!/*.filter { property -> property.visible }*/
+//                    properties.forEach { p -> Log.d("ncs", p.toString()) }
+
+                    setupViewPager(properties)
+                } else {
+                    Toast.makeText(this@SearchActivity, getString(R.string.no_response), Toast.LENGTH_LONG).show()
+                }
+                pg_bar.visibility = ProgressBar.GONE
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -46,11 +90,11 @@ class SearchActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
-    fun setupViewPager() {
+    private fun setupViewPager(properties: List<Property>?) {
 
         searchPagerAdapter = SearchPageAdapter(supportFragmentManager)
 
-        val listSearchFragment = ListSearchFragment()
+        val listSearchFragment = ListSearchFragment.instance(properties = ArrayList(properties))
         val mapFragment = MapSearchFragment()
 
         searchPagerAdapter.fragments = listOf(listSearchFragment, mapFragment)
