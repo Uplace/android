4ba687a9d990499debdc4102e8d2035fe11da80f
nicos <nicosfabroc@gmail.com>
2018-05-24 19:30:07 +0200

    City input controll

--- app/src/main/java/es/uplace/uplace/MainActivity.kt
+++ app/src/main/java/es/uplace/uplace/MainActivity.kt
@@ -5,6 +5,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,33 +13,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setClickListeners()
-
-//        R.array.dwelling_types
-
-    }
-
-    private fun setClickListeners() {
-//        btnBuy.setOnClickListener {
-//            changeSelected(btnBuy)
-//        }
-//        btnRent.setOnClickListener {
-//            changeSelected(btnRent)
-//        }
-//        btnTransfer.setOnClickListener {
-//            changeSelected(btnTransfer)
-//        }
         search_button.setOnClickListener {
-            startActivity(Intent(this, SearchActivity::class.java))
+            if (!isCityValid(city_edit_text.text)) {
+                city_text_input.error = getString(R.string.error_city)
+            } else {
+                val intent: Intent = Intent(this, SearchActivity::class.java)
+                intent.putExtra("city.equals", city_edit_text.text)
+                startActivity(intent)
+            }
         }
+
     }
 
-//    fun changeSelected(btn: Button) {
-//        btn.isSelected = !btn.isSelected
-//        if (btn.isSelected) {
-//            btn.background = resources.getDrawable(R.drawable.transaction_button_pressed, theme)
-//        } else {
-//            btn.background = resources.getDrawable(R.drawable.transaction_button, theme)
-//        }
-//    }
+    private fun isCityValid(text: Editable?): Boolean {
+        return text != null && text.length >= 4
+    }
 }
